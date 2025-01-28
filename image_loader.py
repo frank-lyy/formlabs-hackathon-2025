dee3db47468c3dac5468db2decc34c29af4a2a53
Frank <fle@mit.edu>
2025-01-27 23:59:47 -0500

    pointcloud registration

--- image_loader.py
+++ image_loader.py
@@ -35,30 +35,41 @@ def get_depth(filename, width, height):
     
     return depth_image
 
-def get_mask(filename, width, height):
+def get_color(filename, width, height):
     # Read data from png file
     image = Image.open(filename)
-
-    # Resize the image
     resized_image = image.resize((width, height))  # e.g., (800, 600)
     color_data = np.array(resized_image)
     
-    # Create a mask where the object is colored and the background is white
+    return color_data
+
+def get_mask(filename, width, height):
+    color_data = get_color(filename, width, height)
+
     mask = np.zeros((height, width), dtype=np.bool_)
     mask[color_data[:, :, 0] < 50] = True  # Assuming RGB values
     
-    # # Display the mask
-    # plt.imshow(mask, cmap='gray')
-    # plt.title('Mask')
-    # plt.show()
-
+    for i in range(0, 100):
+        mask[:, i] = False
+    
+    # print(mask.shape)
     return mask
 
 if __name__ == "__main__":
     # Update with your actual .raw file path
     first_file = os.path.join('images', '1_Depth.raw')  # Assuming the raw file is in the images directory
     # view_raw_depth(first_file, width=424, height=240)
-    get_mask("images/1_Color.png", width=424, height=240)
+    image1 = get_color("images/1_Color.png", width=424, height=240)
+    mask1 = get_mask("images/1_Color.png", width=424, height=240)
     second_file = os.path.join('images', '2_Depth.raw')  # Assuming the raw file is in the images directory
     # view_raw_depth(second_file, width=424, height=240)
-    get_mask("images/2_Color.png", width=424, height=240)
+    image2 = get_color("images/2_Color.png", width=424, height=240)
+    mask2 = get_mask("images/2_Color.png", width=424, height=240)
+    image1[~mask1] = 0
+    image2[~mask2] = 0
+
+    plt.subplot(1, 2, 1)
+    plt.imshow(image1)
+    plt.subplot(1, 2, 2)
+    plt.imshow(image2)
+    plt.show()
--- state_estimation.py
+++ state_estimation.py
@@ -1,3 +1,97 @@
-from loader import *
-from pointcloud import *
+from image_loader import *
 import numpy as np
+from pycpd import DeformableRegistration
+import matplotlib.pyplot as plt
+from numpy.testing import assert_array_almost_equal
+from functools import partial
+from mpl_toolkits.mplot3d import Axes3D
+
+def get_pointcloud(filename, width, height):
+    depth_image = get_depth(filename+"_Depth.raw", width, height)
+    mask = get_mask(filename+"_Color.png", width, height)
+    
+    pointcloud = []
+    for i in range(depth_image.shape[0]):
+        for j in range(depth_image.shape[1]):
+            if mask[i, j]:
+                pointcloud.append([i, j, depth_image[i, j]])
+                
+    return np.array(pointcloud)
+
+def visualize(iteration, error, X, Y, ax):
+    plt.cla()
+    ax.scatter(X[:, 0],  X[:, 1], X[:, 2], color='red', label='Target')
+    ax.scatter(Y[:, 0],  Y[:, 1], Y[:, 2], color='blue', label='Transformed Source')
+    ax.text2D(0.87, 0.92, 'Iteration: {:d}'.format(
+        iteration), horizontalalignment='center', verticalalignment='center', transform=ax.transAxes, fontsize='x-large')
+    ax.legend(loc='upper left', fontsize='x-large')
+    plt.draw()
+    plt.pause(0.001)
+
+def normalize_pointcloud(points):
+    centroid = np.mean(points, axis=0)
+    points_centered = points - centroid
+    scale = np.max(np.linalg.norm(points_centered, axis=1))
+    points_normalized = points_centered / scale
+    return points_normalized, centroid, scale
+
+def pc_registration(pointcloud_1, pointcloud_2):
+    pc1_norm, pc1_centroid, pc1_scale = normalize_pointcloud(pointcloud_1)
+    pc2_norm, pc2_centroid, pc2_scale = normalize_pointcloud(pointcloud_2)
+    
+    reg = DeformableRegistration(
+        X=pc2_norm,
+        Y=pc1_norm,
+        beta=2,
+        alpha=3,
+        max_iterations=150
+    )
+    
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    callback = partial(visualize, ax=ax)
+    
+    TY_norm, _ = reg.register(callback)
+    plt.show()
+    
+    TY = (TY_norm * pc2_scale) + pc2_centroid
+    
+    return TY
+
+def label_pointclouds(filename_t1, filename_t2):
+    image1 = get_color(filename_t1+"_Color.png", width=424, height=240)
+    image2 = get_color(filename_t2+"_Color.png", width=424, height=240)
+    pointcloud_1 = get_pointcloud(filename_t1, width=424, height=240)
+    pointcloud_2 = get_pointcloud(filename_t2, width=424, height=240)
+    
+    # Get transformed points (normalization handled in pc_registration)
+    TY = pc_registration(pointcloud_1, pointcloud_2)
+
+    # sanity check
+    assert pointcloud_1.shape[0] == TY.shape[0]
+
+    # sample a few points from each image
+    num_points = 5
+    indices = np.random.choice(pointcloud_1.shape[0], num_points, replace=False)
+    pointcloud_1 = pointcloud_1[indices, :]
+    pointcloud_2 = np.round(TY[indices, :], 0).astype(int)
+    
+    # plot the images
+    plt.subplot(1, 2, 1)
+    plt.imshow(image1)
+    plt.scatter(pointcloud_1[:, 1], pointcloud_1[:, 0])
+    # After your scatter plot line, add:
+    for idx, (x, y, _) in enumerate(pointcloud_1):
+        plt.annotate(f'Point {idx}', (y, x), xytext=(5, 5), textcoords='offset points')
+    
+    plt.subplot(1, 2, 2)
+    plt.imshow(image2)
+    plt.scatter(pointcloud_2[:, 1], pointcloud_2[:, 0])
+    # After your scatter plot line, add:
+    for idx, (x, y, _) in enumerate(pointcloud_2):
+        plt.annotate(f'Point {idx}', (y, x), xytext=(5, 5), textcoords='offset points')
+    
+    plt.show()
+    
+if __name__ == "__main__":
+    label_pointclouds("images/1", "images/2")
