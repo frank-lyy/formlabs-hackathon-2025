f15a43466635d4a125c1090809d427ed64674f71
Frank <fle@mit.edu>
2025-02-02 10:00:37 -0500

    finished

--- planner.py
+++ planner.py
@@ -7,7 +7,7 @@ from sklearn.decomposition import PCA
 from scipy.optimize import curve_fit
 from enum import Enum
 
-ANGLE = np.array([[-1, 0], [0, 0], [0, 1]])
+BEND_TEMPLATE = np.array([[-1, 0], [0, 0], [0, 1]])
 
 class SegmentType(Enum):
     SEGMENT = "red"
@@ -27,7 +27,8 @@ def match_template(pointcloud, template_points, window_size):
     best_error = float('inf')
 
     normalized_pointcloud, _, _ = normalize_pointcloud(pointcloud)
-    normalized_template_points, _, _ = normalize_pointcloud(template_points)
+    # normalized_template_points, _, _ = normalize_pointcloud(template_points)
+    normalized_template_points = template_points
     
     # Fit a curve to the template points
     x_template = np.array([p[0] for p in normalized_template_points])
@@ -46,7 +47,7 @@ def match_template(pointcloud, template_points, window_size):
         
         if error < best_error:
             best_error = error
-            best_match = window
+            best_match = i
     
     return best_match, best_error
 
@@ -106,17 +107,20 @@ def segment_shoelace(points, curvature_threshold=2, loop_threshold=0.005):
     labels = [SegmentType.color(SegmentType.SEGMENT) for _ in range(points.shape[0])]
 
     # Detect bends
-    bend_indices = detect_bends(normalized_points, tree)
-    for bend_idx in bend_indices:
-        labels[bend_idx] = SegmentType.color(SegmentType.BEND)
+    window_size = 20
+    best_index, best_error = match_template(normalized_points, BEND_TEMPLATE, window_size)
+    # print(best_index, best_error)
+    best_index = int(best_index)
+    for i in range(best_index, best_index + window_size):
+        labels[i] = SegmentType.color(SegmentType.BEND)
 
-    # Detect loops
-    loop_indices = detect_loops(normalized_points, tree)
-    for loop_start, loop_end in loop_indices:
-        for i in range(loop_start, loop_end):
-            labels[i] = SegmentType.color(SegmentType.LOOP)
+    # # Detect loops
+    # loop_indices = detect_loops(normalized_points, tree)
+    # for loop_start, loop_end in loop_indices:
+    #     for i in range(loop_start, loop_end):
+    #         labels[i] = SegmentType.color(SegmentType.LOOP)
 
-    labels[-1] = SegmentType.color(SegmentType.END)
+    # labels[-1] = SegmentType.color(SegmentType.END)
 
     return labels
 
